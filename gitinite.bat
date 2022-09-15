b9c9352015c9d88027cbc939fd2c80da855a55e2
CHOSSY Lucas <mistermega52@gmail.com>
2022-09-15 09:53:47 +0200

    [Fast Commands] Some visual improvements

--- gitinite.bat
+++ gitinite.bat
@@ -3,12 +3,15 @@ echo.
 call "C:\custom_commands\gitconf.bat" "%~1"
 echo.
 git init
+git remote add origin "%~2"
+echo.
+call "C:\custom_commands\colorm.bat" I "Git Initialized and Remote Added."
+echo.
 git add .
 git commit -m "[Fast Commands] First Commit"
 git branch "%~3"
 git switch "%~3"
-git remote add origin "%~2"
 git push -f origin "%~3"
 echo.
-call "C:\custom_commands\colorm.bat" I "Git Init."
+call "C:\custom_commands\colorm.bat" I "Git Init.
 endcmd
